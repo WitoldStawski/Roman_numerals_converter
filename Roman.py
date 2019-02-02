4ad7bc9cc5bb0eb21e5f020711503c80aa89c14a
Witold Stawski <witold.r.stawski@gmail.com>
2019-02-02 18:53:50 +0100

    First commit

---
+++ Roman.py
@@ -0,0 +1,32 @@
+#This program convert numbers into roman numerals.
+#Created by Witold Stawski.
+
+class Roman:
+    def int_to_Roman(self, num):
+        val = [
+            1000, 900, 500, 400,
+            100, 90, 50, 40,
+            10, 9, 5, 4,
+            1
+            ]
+        syb = [
+            "M", "CM", "D", "CD",
+            "C", "XC", "L", "XL",
+            "X", "IX", "V", "IV",
+            "I"
+            ]
+        roman_num = ''
+        i = 0
+        while  num > 0:
+            for _ in range(num // val[i]):
+                roman_num += syb[i]
+                num -= val[i]
+            i += 1
+        return roman_num
+
+print("This program convert numbers into roman numerals.")
+print("What number you want to convert?")
+number = input()
+number = int(number)
+
+print(Roman().int_to_Roman(number))
